84540d6d296cb494bc9ede299034140641decfb3
Nhut <nhut@example.com>
2025-03-28 22:38:37 +0700

    code commit

--- frontend/src/pages/Home/Gallary/Gallary.jsx
+++ frontend/src/pages/Home/Gallary/Gallary.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import img1 from "../../../assets/gallary/image1.png";
 import img2 from "../../../assets/gallary/image2.png";
+import img3 from "../../../assets/gallary/image3.png";
+import img4 from "../../../assets/gallary/image4.png";
+import img5 from "../../../assets/gallary/image5.png";
 
 const Gallary = () => {
   return (
@@ -13,23 +16,23 @@ const Gallary = () => {
       <div className="md:grid grid-cols-2 items-center justify-center gap-4">
         <div className="mb-4 md:mb-0">
           <img
-            src={img1}
+            src={img3}
             alt=""
             className="md:h-[720px] w-full mx-auto rounded-sm"
           />
         </div>
         <div className="gap-4 grid grid-cols-2 items-start">
           <div>
-            <img src={img2} alt="" className="md:h-[350px] rounded-sm" />
+            <img src={img1} alt="" className="md:h-[350px] rounded-sm" />
           </div>
           <div>
             <img src={img2} alt="" className="md:h-[350px] rounded-sm" />
           </div>
           <div>
-            <img src={img2} alt="" className="md:h-[350px] rounded-sm" />
+            <img src={img4} alt="" className="md:h-[350px] rounded-sm" />
           </div>
           <div>
-            <img src={img2} alt="" className="md:h-[350px] rounded-sm" />
+            <img src={img5} alt="" className="md:h-[350px] rounded-sm" />
           </div>
         </div>
       </div>
